87f06afcfc81cf9833e4c894560bc034842231fc
Administrator <kerokhaled346@gmail.com>
2024-06-23 16:55:03 +0200

    refresh notes list done

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -13,5 +13,7 @@ class NotesCubit extends Cubit<NotesState> {
   fetchAllNotes() async {
     var noteBox = Hive.box<NoteModel>(kNotesBox);
     notes = noteBox.values.toList();
+
+    emit(NotesSuccess());
   }
 }
